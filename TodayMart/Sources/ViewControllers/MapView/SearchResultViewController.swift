58d1d8e61e762049409772ad9e1b9a2530a2bb2e
Gilwan Ryu <opop_gwhi@naver.com>
2019-04-30 13:44:56 +0900

    서치바 진행중

---
+++ TodayMart/Sources/ViewControllers/MapView/SearchResultViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchResultViewController.swift
+//  TodayMart
+//
+//  Created by Gilwan Ryu on 30/04/2019.
+//  Copyright © 2019 Ry. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultViewController: UIViewController {
+    
+    @IBOutlet weak var searchTableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
