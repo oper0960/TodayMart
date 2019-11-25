b1d76760ec3b3447c8c9e68420229d2cc3f8b2eb
GilwanRyu <gilwan.ryu@buxikorea.com>
2019-11-25 15:04:21 +0900

    네비바 투명 다시 살림

---
+++ TodayMart/Sources/ViewControllers/Base/BaseViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BaseViewController.swift
+//  TodayMart
+//
+//  Created by Buxi on 2019/11/25.
+//  Copyright © 2019 Ry. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
